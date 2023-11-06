988bafb090b01c2d0b00ecb62c6ba6d4acf18fdd
bodasupraveenkumar <119805502+bodasupraveenkumar@users.noreply.github.com>
2023-11-06 14:49:49 +0530

    Update more-than-24-hours.component.ts

--- src/app/components/more-than-24-hours.component.ts
+++ src/app/components/more-than-24-hours.component.ts
@@ -2,9 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CountdownConfig } from 'ngx-countdown';
 
 const CountdownTimeUnits: Array<[string, number]> = [
-  ['Y', 1000 * 60 * 60 * 24 * 365], // years
-  ['M', 1000 * 60 * 60 * 24 * 30], // months
-  ['D', 1000 * 60 * 60 * 24], // days
+
   ['H', 1000 * 60 * 60], // hours
   ['m', 1000 * 60], // minutes
   ['s', 1000], // seconds
